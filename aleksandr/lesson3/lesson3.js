ce69ed0cd8d432b528a3535af3a74ad588bf55f6
voroncov.a2 <voroncov.a2@wildberries.work>
2025-05-09 12:11:32 +0300

    lesson3

---
+++ aleksandr/lesson3/lesson3.js
@@ -0,0 +1,10 @@
+const number1 = 2;
+const number2 = 3;
+
+const result = number1 + number2;
+
+console.log("RESULT sum two numbers is!!! ", result);
+
+const header = document.querySelector("h1");
+
+header.style.color = "green";
